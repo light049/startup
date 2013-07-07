8585710b0fa797d3c4d74804e68798ac5a2cef98
light049 <lilikiki@gmail.com>
2013-07-07 16:58:14 +0000

    modify web.js

--- web.js
+++ web.js
@@ -1,9 +1,19 @@
+/*
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
+});
+*/
+var fs = require('fs');
+
+fs.readFile('index.html', function(err, data){
+    if (err) {
+        throw err;
+    }
+    console.log(data);
 });
 
 var port = process.env.PORT || 5000;
